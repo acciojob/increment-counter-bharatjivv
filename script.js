10d185adf7f6fcefdc635412c6853334d4ae008d
Bharat <hibharatj@gmail.com>
2025-06-17 18:10:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your JS code here. If required.
 // Get elements
-    const counterEl = document.getElementById('counter');
-    const incrementBtn = document.getElementById('incrementBtn');
+const counterEl = document.getElementById('counter');
+const incrementBtn = document.getElementById('incrementBtn');
 
-    let count = 0;
+let count = 0;
 
-    incrementBtn.addEventListener('click', () => {
-      alert(count); 
-      count++; 
-      counterEl.textContent = count; 
-    });
+incrementBtn.addEventListener('click', () => {
+  alert(count); 
+  count++; 
+  counterEl.textContent = count; 
+});
